942e144a51c5d15c84ff45a3cd6c277e07c5269b
maraconti1 <maraconti2015@gmail.com>
2023-05-27 11:28:23 -0300

    Practico N0 finalizado

---
+++ practico0/areaRectangulo.js
@@ -0,0 +1,8 @@
+let base = 5;
+let altura = 3;
+let area = base * altura;
+console.log(area);
+let base2 = 12.5;
+let altura2 = 5.3;
+let area2 = base2 * altura2;
+console.log(area2);
---
+++ practico0/promedio3.js
@@ -0,0 +1,15 @@
+let x1 = 5;
+let y1 = 7;
+let z1 = 9;
+let promedio1 = (x1 + y1 + z1) / 3;
+console.log(promedio1);
+let x2 = -2;
+let y2 = -3;
+let z2 = -8;
+let promedio2 = (x2 + y2 + z2) / 3;
+console.log(promedio2);
+let x3 = -1.2;
+let y3 = 5.3;
+let z3 = -3.3;
+let promedio3 = (x3 + y3 + z3) / 3;
+console.log(promedio3);
