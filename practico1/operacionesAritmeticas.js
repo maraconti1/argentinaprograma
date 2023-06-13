f75dbe478aca692bfa406b1547524c5216d85e7d
maraconti1 <maraconti2015@gmail.com>
2023-06-13 19:08:28 -0300

    Agregue el archivo operacionesAritmeticas

---
+++ practico1/operacionesAritmeticas.js
@@ -0,0 +1,9 @@
+let var1 = 1;
+let var2 = 3;
+let var3 = 19;
+let var4 = 8;
+let varA = var1 + var2;
+let varB = var3 - var4;
+let resultadoFinal = varA * varB;
+let esPar = resultadoFinal %2 === 0;
+console.log("Mis variables iniciales fueron:  %i, %i, %i y %i. La respuesta a verificar si el resultado final es par es: %s",var1,var2,var3,var4,esPar);
