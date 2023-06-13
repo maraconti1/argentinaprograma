3ba886e0e0b6a17151696060cddcb919696d4f2d
maraconti1 <maraconti2015@gmail.com>
2023-06-13 19:11:52 -0300

    Agregue el archivo frutas

---
+++ practico1/frutas.js
@@ -0,0 +1,6 @@
+let frutas = [];
+frutas.push("manzana","banana","naranja","pera","frutilla");
+let ultima = frutas.pop();
+console.log("La ultima fruta agregada fue: " + ultima);
+let primera = frutas.shift();
+console.log("La primera fruta agregada fue: %s",primera);
