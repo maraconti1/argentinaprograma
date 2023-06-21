e5713de00235915b217f20aca2d542cdf57fde5d
maraconti1 <maraconti2015@gmail.com>
2023-06-20 23:54:12 -0300

    Agregue el archivo mayorDeEdad

---
+++ practico2/mayorDeEdad.js
@@ -0,0 +1,4 @@
+const readlineSync = require('readline-sync');
+const edad = readlineSync.question('Cual es tu edad? ');
+if (edad>=18){console.log("Eres mayor de edad");}
+else {console.log("Eres menor de edad")};
