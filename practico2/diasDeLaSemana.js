7fad7d987fee536625f257c108b288c72d9dbfe6
maraconti1 <maraconti2015@gmail.com>
2023-06-20 23:59:49 -0300

    Agregue el archivo diasDeLaSemana

---
+++ practico2/diasDeLaSemana.js
@@ -0,0 +1,29 @@
+const readlineSync = require('readline-sync');
+const numero = readlineSync.question("Ingrese un numero del 1 al 7 representando los dias de la semana : ");
+let num = Number(numero)
+switch (num){
+    case 1:
+        console.log("Hoy es lunes");
+        break;
+    case 2:
+        console.log("Hoy es martes");
+        break;
+    case 3:
+        console.log("Hoy es miercoles");
+        break;
+    case 4:
+        console.log("Hoy es jueves");
+        break;
+    case 5:
+        console.log("Hoy es viernes");
+        break;
+    case 6:
+        console.log("Hoy es sabado");
+        break;
+    case 7:
+        console.log("Hoy es domingo");
+        break; 
+    default:
+        console.log("Numero invalido");                       
+
+}
