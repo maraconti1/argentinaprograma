d6f7d6fce6c0f15d58ba199fa304b43f8476c4f7
maraconti1 <maraconti2015@gmail.com>
2023-06-20 23:55:47 -0300

    Agregue el archivo positivoNegativo

---
+++ practico2/positivoNegativo.js
@@ -0,0 +1,5 @@
+const readlineSync = require('readline-sync');
+const numero = readlineSync.question("Ingrese un numero: ");
+if (numero > 0) {console.log("El numero es positivo") }
+else if (numero < 0) {console.log("El numero es negativo") }
+else {console.log("El numero es cero") };
